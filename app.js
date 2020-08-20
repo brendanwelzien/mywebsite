98d9ba9a400d686d120a4ce44755f14e20d0848f
Brendan Welzien <brendanwelzien@gmail.com>
2020-08-20 12:01:25 -0700

    js functions

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 'use strict'
 
-var yourorder = prompt('Tell me your starbucks order!');
-
-alert('I also like ordering a ' + yourorder);
-
-if (yourorder === 'Nothing'){
-    document.write('<h2>' +'this site is for coffee enthusiasts only!' + yourorder + '</h2>')
-} else { 
-    document.write('<h2>' + 'mmm your ' +  yourorder + ' sounds real tasty!' + '</h2>')
+function yourcoffee(){
+    var coffeeorder = prompt('Tell me your Starbucks order!');
+    alert('I also like ordering a ' + coffeeorder);
+    return coffeeorder;
 }
+var coffeetype= yourcoffee();
+
+function coffeeresponse(){
+   if (coffeetype === 'Latte')
+   document.write ('<h2>' + ' Great choice, I should try a ' + coffeetype + ' too'+ '</h2>')
+   else {
+       document.write('<h2>' + 'Wow, you have an exquisite taste for coffee!' + coffeetype + '</h2>')
+   }
+}
+coffeeresponse();
+
+function coffeebattle(){
+    var wouldyourather = prompt('Would you rather drink an Americano or a Cold Brew?');
+    return wouldyourather;
+}
+var coffeepref = coffeebattle()
+
+function writeToWebPage(coffeewinner){
+    document.write('<h3>' + coffeewinner  + '</h3>');
+}
+
+writeToWebPage (coffeepref)
